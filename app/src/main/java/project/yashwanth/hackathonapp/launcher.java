8badb88d8aa947c772389d23bdff98c1591eeafe
yashwanth <yashwanthnb96@gmail.com>
2018-10-06 11:29:48 +0530

    Google signin added

--- app/src/main/java/project/yashwanth/hackathonapp/launcher.java
+++ app/src/main/java/project/yashwanth/hackathonapp/launcher.java
@@ -1,13 +1,27 @@
 package project.yashwanth.hackathonapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class launcher extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        tv=(TextView)findViewById(R.id.ltv);
+        Typeface tf=Typeface.createFromAsset(getAssets(),"fonts/BerkshireSwash_regular.ttf");
+        tv.setTypeface(tf);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        },3000);
     }
 }
---
+++ app/src/main/java/project/yashwanth/hackathonapp/login.java
@@ -0,0 +1,80 @@
+package project.yashwanth.hackathonapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class login extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    Context mContext;
+    int RC_SIGN_IN=201;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mContext = this;
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account!=null)
+            Toast.makeText(this,"Already logged in",Toast.LENGTH_LONG).show();
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Toast.makeText(mContext,"data",Toast.LENGTH_LONG).show();
+            validate(task);
+        }
+    }
+    public void validate(Task<GoogleSignInAccount> completedTask){
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(mContext,"Signed in successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getApplicationContext(),location.class));
+
+        } catch (ApiException e) {
+
+            Log.w("Google login", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(mContext,"Signed in failed, Please signin to continue",Toast.LENGTH_LONG).show();
+        }
+    }
+}
